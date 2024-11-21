b6ae788f911fd7fda471f12f23f3ede12507f8a2
Matias David Schwerdt <schwerdt.matias@gmail.com>
2024-11-21 10:36:53 -0300

    minishell: improved cat

--- MiniShell/cat.c
+++ MiniShell/cat.c
@@ -1,28 +1,42 @@
-
 #include <stdio.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <errno.h>
 
 #define MAX 1000
 
-void main(int argc,char *param[]){
-
+void main(int argc, char *param[]) {
     char dir[100];
-        
-    if(param[1]==NULL){
-        printf("Falta un introducir un parametro.\n \n");
-    } else {
-        strncpy(dir, param[1], sizeof(dir));
-	FILE *f = fopen(dir, "r");
-	char leido[MAX];
-	if (f != NULL){
-		while (fgets(leido, MAX, f) != NULL){
-			printf(leido);
-		}
-	}
-	else {  
-		printf("Error al abrir el archivo");
-          }
-	fclose(f);
-        
+    
+    if (param[1] == NULL) {
+        printf("Falta un introducir un parametro.\n\n");
+        return;
     }
+
+    strncpy(dir, param[1], sizeof(dir) - 1);
+    dir[sizeof(dir) - 1] = '\0'; // Asegurar terminación nula
+
+    struct stat fileStat;
+    if (stat(dir, &fileStat) != 0) {
+        perror("Error: el archivo no existe");
+        return;
+    }
+
+    if (S_ISDIR(fileStat.st_mode)) {
+        printf("Error: El parámetro corresponde a un directorio, no un archivo.\n\n");
+        return;
+    }
+
+    FILE *f = fopen(dir, "r");
+    if (f == NULL) {
+        perror("Error al abrir el archivo");
+        return;
+    }
+
+    char leido[MAX];
+    while (fgets(leido, MAX, f) != NULL) {
+        printf("%s", leido);
+    }
+
+    fclose(f);
 }
